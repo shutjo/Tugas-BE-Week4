c3a4d6d6ae9434a36527b9340bb211eb0347f57a
shutjo <mrjoko1997@gmail.com>
2023-02-21 14:56:50 +0700

    commit

--- src/ListCollections.java
+++ src/ListCollections.java
@@ -1,2 +1,68 @@
-public class ListCollection {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ListCollections {
+    public static void main(String[] args) {
+        //a. List Nama makanan minimal 10 data
+        ArrayList<String> food = new ArrayList<>();
+        food.add("Nasi Goreng");
+        food.add("Soto");
+        food.add("Ketoprak");
+        food.add("Nasi Uduk");
+        food.add("Bubur Ayam");
+        food.add("Pecel Ayam");
+        food.add("Bakso");
+        food.add("Mie Ayam");
+        food.add("Lontong Sayur");
+        food.add("Sop Buntut");
+        System.out.println(food);
+
+        //b. List Tahun Piala Dunia minimal 3 data
+        List<Integer> wordCup = Arrays.asList(2010,2014,2018);
+        System.out.println(wordCup);
+
+        //c. Implement sort pada list nama provinsi minimal 10 data secara descending
+        ArrayList<String> province = new ArrayList<>();
+        province.add("DKI Jakarta");
+        province.add("Jawa Barat");
+        province.add("Jawa Tengah");
+        province.add("Daerah Istimewa Yogyakarta");
+        province.add("Jawa Timur");
+        province.add("Bali");
+        province.add("Nusa Tengara Timur");
+        province.add("Sumatra Selatan");
+        province.add("Kalimantan Selatan");
+        province.add("Sulawesi Selatan");
+        province.sort(Collections.reverseOrder());
+        System.out.println(province);
+
+        //d. Implement penghapusan data untuk 5 data yang berbeda dari list bahasa pemograman yang berisikan 12 data
+        ArrayList<String> language = new ArrayList<>();
+        language.add("JAVA");
+        language.add("PHP");
+        language.add("Python");
+        language.add("Go");
+        language.add("Ruby");
+        language.add("C++");
+        language.add("TypeScript");
+        language.add("JavaScript");
+        language.add("Kotlin");
+        language.add("Swift");
+        language.add("Dart");
+        language.add("C#");
+        System.out.println(language);
+
+        System.out.println("SETELAH DI REMOVE");
+        language.remove(1);
+        language.remove(0);
+        language.remove(5);
+        language.remove(7);// remove parameter index
+        language.remove("Dart");// remove parameter value
+
+        for(String output : language){
+            System.out.println(output);
+        }
+    }
 }
--- src/RegExChecker.java
+++ src/RegExChecker.java
@@ -1,5 +1,36 @@
-public class Main {
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class RegExChecker {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan yang ingin diperiksa: ");
+        String input = scanner.nextLine();
+        checkPattern(input);
+    }
+    public static boolean checkPattern(String input) {
+        String email = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+        String date1 = "\\d{2}-\\d{2}-\\d{4}";
+        String date2 = "\\p{L}{4,10},\\s\\p{L}{4,10}\\s\\d{2}";
+        String ipAddress = "^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$";
+
+        boolean isEmail = Pattern.matches(email, input);
+        boolean isDate1 = Pattern.matches(date1, input);
+        boolean isDate2 = Pattern.matches(date2, input);
+        boolean isIPAddress = Pattern.matches(ipAddress, input);
+
+        if (isEmail) {
+            System.out.println("Ini adalah email");
+            return true;
+        } else if (isDate1 || isDate2) {
+            System.out.println("Ini adalah date");
+            return true;
+        } else if (isIPAddress) {
+            System.out.println("Ini adalah  IpAddress");
+            return true;
+        } else {
+            System.err.println("Tidak ada yang sesuai");
+            return false;
+        }
     }
 }
